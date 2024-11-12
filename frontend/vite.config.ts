878b1911e111169191b4e6d7dfab19b0aa688f79
NRI酒井 将大 <m2-sakai@nri.co.jp>
2024-11-12 12:12:18 +0900

    feat: add remix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,25 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
+import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+declare module "@remix-run/node" {
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
 
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+  plugins: [
+    remix({
+      ssr: false,
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
