247653d3688283838dc80f8c2bd4a596f83bcd8c
NRI酒井 将大 <m2-sakai@nri.co.jp>
2024-12-19 08:17:28 +0900

    fix: wss url

--- frontend/app/features/chats/api/route.ts
+++ frontend/app/features/chats/api/route.ts
@@ -4,5 +4,5 @@ const VITE_WSS_URL = import.meta.env.VITE_WSS_URL;
 export const API_ROUTES = {
   GET_TOKEN: `${VITE_REST_URL}/token`,
   LIST_CHAT: `${VITE_REST_URL}/chats`,
-  WSS_CONNECT: `${VITE_WSS_URL}/client/hubs/chatroom`,
+  WSS_CONNECT: `${VITE_WSS_URL}/websocket`,
 };
